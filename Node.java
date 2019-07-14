db0f437ef82e6e71ff5cacdaf58ca4cdc9980576
aidan.d94 <aidan.d94@gmail.com>
2019-07-14 23:18:32 +0100

    Adding getter/setter/constructor for Edge and Node

--- Node.java
+++ Node.java
@@ -4,4 +4,42 @@ public class Node{
     private int value;
     private HashSet<Edge> inEdges;
     private HashSet<Edge> outEdges;
+
+    public Node(int value) {
+        this.value = value;
+        inEdges = new HashSet<Edge>();
+        outEdges = new HashSet<Edge>();
+    }
+
+    public HashSet<Edge> getInEdges() {
+        return inEdges;
+    }
+
+    public HashSet<Edge> getOutEdges() {
+        return outEdges;
+    }
+
+    public int getValue() {
+        return this.value = value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setInEdges(HashSet<Edge> inEdges) {
+        this.inEdges = inEdges;
+    }
+
+    public void setOutEdges(HashSet<Edge> outEdges) {
+        this.outEdges = outEdges;
+    }
+
+    public Node addEdge(Node node){
+        Edge e = new Edge(this, node);
+        outEdges.add(e);
+        node.inEdges.add(e);
+
+        return this;
+    }
 }
